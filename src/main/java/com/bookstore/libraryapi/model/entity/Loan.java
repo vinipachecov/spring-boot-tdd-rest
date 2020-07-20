a9610d957eed064e6641f135229ea8c18e5e659d
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-20 20:24:36 -0300

    test: ensure LoanRepository to return true if book is in a loan and not returned

--- src/main/java/com/bookstore/libraryapi/model/entity/Loan.java
+++ src/main/java/com/bookstore/libraryapi/model/entity/Loan.java
@@ -5,16 +5,29 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Loan {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column
     private String customer;
+
+    @JoinColumn(name ="id_book")
+    @ManyToOne
     private Book book;
+
+    @Column
     private LocalDate loanDate;
+
+    @Column
     private Boolean returned;
 }
