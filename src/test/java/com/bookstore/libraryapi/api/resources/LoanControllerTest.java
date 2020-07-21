7b9fb9ac025f138b379cee5fb08bc9a3dd15b01a
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-07-20 21:23:52 -0300

    test: ensure loan Filter Route filters returns an array of loans when requested

--- src/test/java/com/bookstore/libraryapi/api/resources/LoanControllerTest.java
+++ src/test/java/com/bookstore/libraryapi/api/resources/LoanControllerTest.java
@@ -1,6 +1,7 @@
 package com.bookstore.libraryapi.api.resources;
 
 import com.bookstore.libraryapi.api.dto.LoanDto;
+import com.bookstore.libraryapi.api.dto.LoanFilterDTO;
 import com.bookstore.libraryapi.api.dto.ReturnedLoanDTO;
 import com.bookstore.libraryapi.api.resource.LoanController;
 import com.bookstore.libraryapi.exception.BusinessException;
@@ -8,6 +9,7 @@ import com.bookstore.libraryapi.model.entity.Book;
 import com.bookstore.libraryapi.model.entity.Loan;
 import com.bookstore.libraryapi.service.BookService;
 import com.bookstore.libraryapi.service.LoanService;
+import com.bookstore.libraryapi.service.LoanServiceTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.weaver.patterns.IVerificationRequired;
@@ -22,6 +24,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -29,9 +34,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 
+import static com.bookstore.libraryapi.api.resources.BookControllerTest.BOOK_API;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -148,6 +156,30 @@ public class LoanControllerTest {
                         .content(json)
         )
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("should filter loans")
+    public void findLoansTest() throws Exception {
+        Long id = 1L;
+        Loan loan = LoanServiceTest.createValidLoan();
+        loan.setId(id);
+
+        BDDMockito.given( loanService.find(Mockito.any(LoanFilterDTO.class), Mockito.any(Pageable.class)))
+                .willReturn(new PageImpl<Loan >(Arrays.asList(loan), PageRequest.of(0, 10), 1));
+
+        String queryString = String.format("?isbn=%s&customer=%s&page=0&size=10", loan.getBook().getIsbn(), loan.getCustomer());
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get(LOAN_API.concat(queryString))
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc
+                .perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("content", hasSize(1)))
+                .andExpect(jsonPath("totalElements").value(1))
+                .andExpect(jsonPath("pageable.pageSize").value((10)))
+                .andExpect(jsonPath("pageable.pageNumber").value(0));
     }
 
 
